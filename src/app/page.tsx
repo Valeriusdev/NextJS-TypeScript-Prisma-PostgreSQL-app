4231f837edaa2f6323b517b83d15612ef003495e
Val Lysenko <kakao757@gmail.com>
2025-09-10 20:27:14 -0700

    update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
     <HydrateClient>
       <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center gap-4 px-4 pt-8 pb-4">
-          <div className="w-full max-w-7xl mb-20">
+          <div className="mb-16 w-full max-w-7xl">
             {/* <p className="text-2xl text-white">
               {hello ? hello.greeting : "Loading tRPC query..."}
             </p> */}
@@ -50,7 +50,7 @@ export default async function Home() {
               </Link>
             </div>
           </div>
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <h1 className="mb-10 text-5xl font-extrabold tracking-tight sm:text-[5rem]">
             <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 lg:grid-cols-4 lg:grid-rows-2">
